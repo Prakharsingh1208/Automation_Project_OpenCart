56d3722533635f4ee22fad46982ac996c1fda6ab
prakharsingh1208 <prakharsingh1208@gmail.com>
2025-03-19 14:40:48 +0530

    Adding allure steps

--- base_page/unit/cart.py
+++ base_page/unit/cart.py
@@ -2,6 +2,8 @@ import logging
 import time
 from time import sleep
 
+import allure
+
 from utilities.logs_genrator import LogsGenrator
 import pytest
 from selenium import webdriver
@@ -15,26 +17,32 @@ class Cart:
     def __init__(self,driver):
         self.driver=driver
 
+    @allure.step("Adding product to cart from home page")
     def adding_to_cart_homepage(self,add_to_cart_location):
         self.logs.info(f"adding product to cart from homepage")
         self.driver.find_element(By.XPATH,add_to_cart_location).click()
 
+    @allure.step("adding product to cart from product page")
     def adding_to_cart_product_page(self,add_to_cart_location):
         self.logs.info(f"adding product to cart from product description")
         self.driver.find_element(By.XPATH,add_to_cart_location).click()
 
+    @allure.step("adding product to cart from search page")
     def adding_to_cart_search_page(self,add_to_cart_location):
         self.logs.info(f"adding product to cart from search page")
         self.driver.find_element(By.XPATH,add_to_cart_location).click()
 
+    @allure.step("Clicking on 'add to cart' button")
     def clicking_black_cart_button(self,add_to_cart_location):
         self.logs.info(f"Clicking on 'add to cart' button")
         self.driver.find_element(By.XPATH,add_to_cart_location).click()
 
+    @allure.step("Adding product to cart from product page")
     def clicking_product_page(self,product_page_location):
         self.logs.info(f"going to product page")
         self.driver.find_element(By.XPATH,product_page_location).click()
 
+    @allure.step("Validating the cart")
     def verify_the_empty_cart(self,black_cart_info,empty_cart_message,success_prompt_location,test_case_id):
         info = self.driver.find_element(By.XPATH,black_cart_info).text.split()
         time.sleep(1)
@@ -47,6 +55,7 @@ class Cart:
             self.logs.info(f"{test_case_id} Passed !")
             assert True
 
+    @allure.step("Validating the cart")
     def verify_multiple_item_in_cart(self,black_cart_button_item_lists,test_case_id):
         self.logs.info(f"Verifying if we can add multiple items in cart")
         self.driver.execute_script("return document.querySelectorAll('table.table-striped tr')")
